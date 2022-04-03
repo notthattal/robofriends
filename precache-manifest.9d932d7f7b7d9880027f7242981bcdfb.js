dab842d30ea19dd84b575cae8e254823e772c122
Tal Erez <talerez525@gmail.com>
2022-04-03 18:16:13 -0400

    Updates

---
+++ precache-manifest.9d932d7f7b7d9880027f7242981bcdfb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "acc358e76b8c9a2aef90",
+    "url": "/robofriends/static/css/main.e4053da5.chunk.css"
+  },
+  {
+    "revision": "acc358e76b8c9a2aef90",
+    "url": "/robofriends/static/js/main.acc358e7.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/robofriends/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "a570caf4c0f4bd95fc85",
+    "url": "/robofriends/static/css/2.46812701.chunk.css"
+  },
+  {
+    "revision": "a570caf4c0f4bd95fc85",
+    "url": "/robofriends/static/js/2.a570caf4.chunk.js"
+  },
+  {
+    "revision": "69d7087b8b0ba8fee442",
+    "url": "/robofriends/static/js/runtime~main.69d7087b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "12fb0489113776403fcc56d498ea43e1",
+    "url": "/robofriends/index.html"
+  }
+];
